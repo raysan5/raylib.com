06600cd3b0db02d81e000cd5a3b27be9b72eef95
dennis ko <dennis0324@naver.com>
2023-09-24 17:40:54 +0900

    docs: translate raylib_color to kr

--- cheatsheet/kr/raylib_colors.c
+++ cheatsheet/kr/raylib_colors.c
@@ -1,28 +1,28 @@
-    // Custom raylib color palette for amazing visuals on WHITE background
-    #define LIGHTGRAY  (Color){ 200, 200, 200, 255 }   // Light Gray
-    #define GRAY       (Color){ 130, 130, 130, 255 }   // Gray
-    #define DARKGRAY   (Color){ 80, 80, 80, 255 }      // Dark Gray
-    #define YELLOW     (Color){ 253, 249, 0, 255 }     // Yellow
-    #define GOLD       (Color){ 255, 203, 0, 255 }     // Gold
-    #define ORANGE     (Color){ 255, 161, 0, 255 }     // Orange
-    #define PINK       (Color){ 255, 109, 194, 255 }   // Pink
-    #define RED        (Color){ 230, 41, 55, 255 }     // Red
-    #define MAROON     (Color){ 190, 33, 55, 255 }     // Maroon
-    #define GREEN      (Color){ 0, 228, 48, 255 }      // Green
-    #define LIME       (Color){ 0, 158, 47, 255 }      // Lime
-    #define DARKGREEN  (Color){ 0, 117, 44, 255 }      // Dark Green
-    #define SKYBLUE    (Color){ 102, 191, 255, 255 }   // Sky Blue
-    #define BLUE       (Color){ 0, 121, 241, 255 }     // Blue
-    #define DARKBLUE   (Color){ 0, 82, 172, 255 }      // Dark Blue
-    #define PURPLE     (Color){ 200, 122, 255, 255 }   // Purple
-    #define VIOLET     (Color){ 135, 60, 190, 255 }    // Violet
-    #define DARKPURPLE (Color){ 112, 31, 126, 255 }    // Dark Purple
-    #define BEIGE      (Color){ 211, 176, 131, 255 }   // Beige
-    #define BROWN      (Color){ 127, 106, 79, 255 }    // Brown
-    #define DARKBROWN  (Color){ 76, 63, 47, 255 }      // Dark Brown
+    // 흰색 배경 위에서의 환상적인 시각적 효과를 위한 커스텀 raylib 색상 팔레트
+    #define LIGHTGRAY  (Color){ 200, 200, 200, 255 }   // 밝은 회색
+    #define GRAY       (Color){ 130, 130, 130, 255 }   // 회색
+    #define DARKGRAY   (Color){ 80, 80, 80, 255 }      // 진한 회색
+    #define YELLOW     (Color){ 253, 249, 0, 255 }     // 노랑색
+    #define GOLD       (Color){ 255, 203, 0, 255 }     // 금색
+    #define ORANGE     (Color){ 255, 161, 0, 255 }     // 오렌지색
+    #define PINK       (Color){ 255, 109, 194, 255 }   // 핑크색
+    #define RED        (Color){ 230, 41, 55, 255 }     // 빨간색
+    #define MAROON     (Color){ 190, 33, 55, 255 }     // 밤색
+    #define GREEN      (Color){ 0, 228, 48, 255 }      // 초록색
+    #define LIME       (Color){ 0, 158, 47, 255 }      // 초록색
+    #define DARKGREEN  (Color){ 0, 117, 44, 255 }      // 진한 초록색
+    #define SKYBLUE    (Color){ 102, 191, 255, 255 }   // 하늘색
+    #define BLUE       (Color){ 0, 121, 241, 255 }     // 파란색
+    #define DARKBLUE   (Color){ 0, 82, 172, 255 }      // 진한 파란색
+    #define PURPLE     (Color){ 200, 122, 255, 255 }   // 보라색
+    #define VIOLET     (Color){ 135, 60, 190, 255 }    // 남보라색
+    #define DARKPURPLE (Color){ 112, 31, 126, 255 }    // 진한 보라색
+    #define BEIGE      (Color){ 211, 176, 131, 255 }   // 베이지색
+    #define BROWN      (Color){ 127, 106, 79, 255 }    // 갈색
+    #define DARKBROWN  (Color){ 76, 63, 47, 255 }      // 진한 갈색
 
-    #define WHITE      (Color){ 255, 255, 255, 255 }   // White
-    #define BLACK      (Color){ 0, 0, 0, 255 }         // Black
-    #define BLANK      (Color){ 0, 0, 0, 0 }           // Blank (Transparent)
-    #define MAGENTA    (Color){ 255, 0, 255, 255 }     // Magenta
-    #define RAYWHITE   (Color){ 245, 245, 245, 255 }   // My own White (raylib logo)
+    #define WHITE      (Color){ 255, 255, 255, 255 }   // 흰색
+    #define BLACK      (Color){ 0, 0, 0, 255 }         // 검정색
+    #define BLANK      (Color){ 0, 0, 0, 0 }           // 검정색 (투명)
+    #define MAGENTA    (Color){ 255, 0, 255, 255 }     // 마젠타
+    #define RAYWHITE   (Color){ 245, 245, 245, 255 }   // raylib 전용 색상 (raylib 로고)
