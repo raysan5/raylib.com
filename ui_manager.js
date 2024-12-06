22cf940cf56d34f390efcd8e8259a3ef4f81b611
treblig-punisher <gilbertplayx@hotmail.com>
2024-12-06 18:52:48 -0500

    replaced the bindings id from all elements using it as a class, and made it into a class.

--- ui_manager.js
+++ ui_manager.js
@@ -54,8 +54,103 @@ class GoToTopButton extends HTMLElement {
       window.scrollTo(0, 0);
     });
     window.addEventListener("scroll", () => {
-        this.classList.toggle("spring-anim-class", window.scrollY > 200);
+      this.classList.toggle("spring-anim-class", window.scrollY > 200);
+    });
+  }
+}
+const hamburgerMenuElem = `<button id="hamburger-button">
+<svg  width="100%" height="100%" viewBox="0 0 242 181" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;"><g id="hamburger"><path d="M241.667,20.885c-0,11.526 -9.358,20.885 -20.885,20.885l-199.897,-0c-11.527,-0 -20.885,-9.359 -20.885,-20.885c0,-11.527 9.358,-20.885 20.885,-20.885l199.897,0c11.527,0 20.885,9.358 20.885,20.885Z" style="fill:currentColor;"/><path d="M241.667,90.306c-0,11.527 -9.358,20.885 -20.885,20.885l-199.897,-0c-11.527,-0 -20.885,-9.358 -20.885,-20.885c0,-11.527 9.358,-20.885 20.885,-20.885l199.897,0c11.527,0 20.885,9.358 20.885,20.885Z" style="fill:currentColor;"/><path d="M241.667,159.727c-0,11.527 -9.358,20.885 -20.885,20.885l-199.897,-0c-11.527,-0 -20.885,-9.358 -20.885,-20.885c0,-11.527 9.358,-20.885 20.885,-20.885l199.897,0c11.527,0 20.885,9.358 20.885,20.885Z" style="fill:currentColor;"/></g></svg>
+</button>`;
+
+class HamburgerMenuButton extends HTMLElement {
+  constructor() {
+    super();
+    const shadow = this.attachShadow({ mode: "open" });
+    const style = document.createElement("style");
+    const hamBurgerButtonTemplate = document.createElement("template");
+    hamBurgerButtonTemplate.innerHTML = hamburgerMenuElem;
+    style.textContent = `
+
+      // :host{
+      //   display: flex;
+      //   margin-left: auto;
+      // }
+      #hamburger-button{    
+        --max-button-size: 60px;
+        display: flex;
+        margin-left: auto;
+        margin-right: 5%;
+        position: relative;
+        max-height: var(--max-button-size);
+        max-width: var(--max-button-size);
+        border: none;
+        background-color: transparent;
+        padding: 10px;
+        
+        transition: background-color 0.3s ease;
+        &:hover{
+            cursor: pointer;
+            color: white;
+            background-color: #111111;
+        }
+          @media only screen and (width > 850px){                  
+            display: none;   
+            --mobile-menu-is-open: "false";                   
+          }
+      }`;
+    this.mobileMenu = document.querySelector("hamburger-menu-button")
+    shadow.appendChild(hamBurgerButtonTemplate.content.cloneNode(true));
+    shadow.appendChild(style);
+    this.addEventListener("click", () => {
+      alert(this.mobileMenu)
+      this.mobileMenu.style.display = "flex";
     })
   }
 }
+
+const mobileMenuHTML = `
+<div class="nav-mobile-container" data-open="false">
+        <div id="mobile-title-bar">
+                <div id="mobile-logo">
+                        <img src="common/img/raylib_logo.png" alt="">
+                </div>
+                <button id="mobile-close-button"></button>
+        </div>
+        <nav class="mobile-menu">
+                <a id="mobile-about" href="" target="_blank">about</a>
+                <a id="mobile-examples" href="" target="_blank">examples</a>
+                <a id="mobile-games" href="" target="_blank">games</a>
+                <a id= "mobile-cheatsheet" href="cheatsheet/cheatsheet.html" target="_blank">cheatsheet</a>
+                <a id="mobile-wiki" href="https://github.com/raysan5/raylib/wiki" target="_blank">wiki</a>
+        </nav>
+        <div id="mobile-social">
+          <a id="mobile-twitter" href="https://www.twitter.com/raysan5" target="_blank" aria-label="raysan5's Twitter"><img src="common/img/icon_twitter.png" /></a>
+          <a id="mobile-discord" href="https://discord.gg/raylib" target="_blank" aria-label="raylib discord"><img src="common/img/icon_discord.png" ></a>
+          <a id="mobile-twitch" href="https://www.twitch.tv/raysan5" target="_blank" aria-label="raysan5 twitch channel"><img src="common/img/icon_twitch.png" /></a>
+          <a id="mobile-kofi" href="https://ko-fi.com/raysan5" target="_blank" aria-label="raylib coffee page for support"><img src="common/img/icon_kofi.png" /></a>
+          <a id="mobile-itchio" href="https://raysan5.itch.io" target="_blank" aria-label="raylib itch.io page"><img src="common/img/icon_itchio.png" ></a>
+          <a id="mobile-handmade" href="https://raylib.handmade.network/" target="_blank" aria-label="raylib handmadenetwork"><img src="common/img/icon_handmade.png" /></a>
+          <a id="mobile-reddit" href="https://www.reddit.com/r/raylib/" target="_blank" aria-label="raylib reddit"><img src ="common/img/icon_reddit.png"/></a>
+          <a id="mobile-youtube" href="https://www.youtube.com/c/raylib" target="_blank" aria-label="raylib youtube channel"><img src="common/img/icon_youtube.png" /></a>
+          <a id="mobile-patreon" href="https://www.patreon.com/raylib" target="_blank" aria-label="ray's patreon page"><img src="common/img/icon_patreon.png" /></a>
+          <a id="mobile-redbubble" href="https://www.redbubble.com/shop/ap/78130012" target="_blank" aria-label="red bubble"><img src="common/img/icon_redbubble.png" /></a>
+          <a id="mobile-github" href="https://github.com/raysan5/raylib" target="_blank" aria-label="raylib's github"><img src="common/img/icon_github.png" /></a>
+          <a id="mobile-bluesky" href="https://bsky.app/profile/raysan5.bsky.social" target="_blank" aria-label="raylib's bluesky profile"><img src="common/img/icon_bluesky.png" /></a>
+        </div>
+</div>
+`
+class MobileMenu extends HTMLElement {
+  constructor() {
+    super();
+    const shadow = this.attachShadow({ mode: "open" });
+    const style = document.createElement("style");
+    style.textContent = ``;
+    const menuTemplate = document.createElement("template");
+    menuTemplate.innerHTML = mobileMenu;
+    shadow.appendChild(menuTemplate.content.cloneNode(true));
+    shadow.appendChild(style);
+    
+  }
+}
 customElements.define("go-to-top-button", GoToTopButton);
+customElements.define("hamburger-menu-button", HamburgerMenuButton);
