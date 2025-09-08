02cfd6e01077dd9c80bafe2daa977d9d6ec4210a
Ray <raysan5@gmail.com>
2025-09-08 20:28:18 +0200

    REXM: RENAME: example: `shapes_draw_circle_sector` --> `shapes_circle_sector_drawing`

--- common/examples.js
+++ common/examples.js
@@ -104,7 +104,7 @@ $(document).ready(function() {
         exampleEntry('⭐⭐☆☆', 'shapes', 'easings_box_anim'),
         exampleEntry('⭐⭐⭐☆', 'shapes', 'easings_rectangle_array'),
         exampleEntry('⭐⭐⭐☆', 'shapes', 'ring_drawing'),
-        exampleEntry('⭐⭐⭐☆', 'shapes', 'draw_circle_sector'),
+        exampleEntry('⭐⭐⭐☆', 'shapes', 'circle_sector_drawing'),
         exampleEntry('⭐⭐⭐☆', 'shapes', 'rounded_rectangle_drawing'),
         exampleEntry('⭐⭐⭐⭐️', 'shapes', 'top_down_lights'),
         exampleEntry('⭐⭐⭐⭐️', 'shapes', 'rectangle_advanced'),
