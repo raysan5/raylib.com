e4e9445aafc991029f6f47302d02ca510b062804
Ray <raysan5@gmail.com>
2025-09-08 20:33:19 +0200

    REXM: RENAME: example: `shaders_normal_map` --> `shaders_normalmap_rendering`

--- common/examples.js
+++ common/examples.js
@@ -190,7 +190,7 @@ $(document).ready(function() {
         exampleEntry('⭐⭐⭐☆', 'shaders', 'hot_reloading'),
         exampleEntry('⭐⭐⭐⭐️', 'shaders', 'mesh_instancing'),
         exampleEntry('⭐⭐☆☆', 'shaders', 'multi_sample2d'),
-        exampleEntry('⭐⭐⭐⭐️', 'shaders', 'normal_map'),
+        exampleEntry('⭐⭐⭐⭐️', 'shaders', 'normalmap_rendering'),
         exampleEntry('⭐⭐☆☆', 'shaders', 'spotlight'),
         exampleEntry('⭐⭐⭐⭐️', 'shaders', 'deferred_rendering'),
         exampleEntry('⭐⭐⭐⭐️', 'shaders', 'hybrid_rendering'),
